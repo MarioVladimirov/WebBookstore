64be887dd5a361e2fa28b141aba5c91a7f901184
nzlateva <72468080+nzlateva@users.noreply.github.com>
2021-09-22 13:20:19 +0300

    Initial commit.

---
+++ src/main/java/bg/softuni/webbookstore/WebBookstoreApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.webbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/webbookstore/WebBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.webbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
