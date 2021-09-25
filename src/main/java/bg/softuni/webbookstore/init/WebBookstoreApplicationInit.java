9352791b63ebdcc6173150638cec2317f00a33bf
nzlateva <72468080+nzlateva@users.noreply.github.com>
2021-09-25 10:34:39 +0300

    Added layers to project structure.

---
+++ src/main/java/bg/softuni/webbookstore/init/WebBookstoreApplicationInit.java
@@ -0,0 +1,13 @@
+package bg.softuni.webbookstore.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebBookstoreApplicationInit implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
