caab26d8f3908ae477aae9f779a408fff4335281
nzlateva <72468080+nzlateva@users.noreply.github.com>
2021-11-03 00:31:01 +0200

    Placed js scripts that are not common only in the respective templates.

---
+++ src/main/resources/static/js/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.btn.btn-danger').on('click', function () {
+        $("#deleteConfirmModal").modal("toggle");
+    });
+});
+
+$(document).ready(function () {
+    $('.btn-close').on('click', function () {
+        $("#deleteConfirmModal").modal("toggle");
+    });
+});
